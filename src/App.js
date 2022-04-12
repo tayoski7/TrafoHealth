e6a1fa6f11e345758df9bf299923b04a8426ad4a
tayo <tayoski7@gmail.com>
2022-04-12 19:06:02 +0100

    npm run build added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ function App() {
 export default App
 
 
+
+
+
   {/* <div style={{width:'100%', height:'500px', display:'flex',justifyContent:'center'}}>
       <iframe width="90%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=23%20abiodun%20obayemi%20ikola&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
       <br/>
